925e2a75d687f6e8e4590dad2d25ec5d3db77f4e
Twisha21 <85494862+Twisha21@users.noreply.github.com>
2021-07-20 19:19:02 +0530

    Add files via upload

---
+++ computerPlayer.js
@@ -0,0 +1,26 @@
+class ComputerPlayer { 
+
+    constructor (x, y, width, height) {
+        var op = {
+            isStatic : true
+        }
+        this.image = loadImage ("assets/player.png");
+        this.width = width; 
+        this.height = height; 
+        this.body = Bodies.rectangle(x,y,this.width,this.height,op); 
+        World.add(world,this.body);
+    }
+
+    display (){
+        var pos = this.body.position; 
+        var angle = this.body.angle; 
+        
+        push ();
+        translate (pos.x,pos.y);
+        rotate (angle);
+        imageMode(CENTER);  
+        image( this.image, 0, 0, this.width, this.height);
+        pop (); 
+    }
+        
+}
---
+++ playerBase.js
@@ -0,0 +1,26 @@
+class PlayerBase { 
+
+    constructor (x, y, width, height) {
+        var op = {
+            isStatic : true
+        }
+        this.image = loadImage ("assets/base2.png");
+        this.width = width; 
+        this.height = height; 
+        this.body = Bodies.rectangle(x,y,this.width,this.height,op); 
+        World.add(world,this.body);
+    }
+
+    display (){
+        var pos = this.body.position; 
+        var angle = this.body.angle; 
+        
+        push ();
+        translate (pos.x,pos.y);
+        rotate (angle);
+        imageMode(CENTER);  
+        image( this.image, 0, 0, this.width, this.height);
+        pop (); 
+    }
+        
+}
